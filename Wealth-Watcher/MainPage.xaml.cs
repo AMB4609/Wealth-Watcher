63a6d165c30c58bad536f590cfc81a0ac6b2af2f
AMB4609 <amoghbajracharya@gmail.com>
2024-12-15 21:15:11 +0545

    initial project setup

---
+++ Wealth-Watcher/MainPage.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace Wealth_Watcher
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ Wealth-Watcher/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace Wealth_Watcher
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
